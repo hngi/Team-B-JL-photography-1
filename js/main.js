42ec546fc097335c09cd702f6e2060d55bf7fd9b
auleki <kodagiwa@gmail.com>
2020-07-03 03:31:23 +0100

    responsiveness completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-const nav = document.querySelector('.')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav')
+
+function showMenu () {
+
+}
+
+burger.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(nav.className);
+    if (nav.className === '') {
+        nav.className += 'responsive';
+    } else {
+        nav.className = ''
+    }
+
+
+})
